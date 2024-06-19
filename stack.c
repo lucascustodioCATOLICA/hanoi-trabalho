bc096eee21e7e64a5a9899f0bbeb7759ad4d7bef
pedro luis <142275228+joseliaaquino1@users.noreply.github.com>
2024-06-19 20:19:28 -0300

    Update stack.c

--- stack.c
+++ stack.c
@@ -8,3 +8,15 @@ struct stack {
 	int itens[TAM_MAX];
 	int tp;
 };
+
+Stack * criar() {
+	Stack * p = (Stack *)malloc(sizeof(Stack));
+	p->tp = 0;
+	return p;
+}
+
+void destruir(Stack * p) {
+	if (p != NULL) {
+		free(p);
+	}
+}
